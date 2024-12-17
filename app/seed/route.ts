70fd61fd9e414d5214598b6451a6cf450643bf0e
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2024-12-17 18:52:50 +0900

    ７章まで

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,17 +102,16 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  try {
-    await client.sql`BEGIN`;
-    await seedUsers();
-    await seedCustomers();
-    await seedInvoices();
-    await seedRevenue();
-    await client.sql`COMMIT`;
-
-    return Response.json({ message: "Database seeded successfully" });
-  } catch (error) {
-    await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
-  }
+  // try {
+  //   await client.sql`BEGIN`;
+  //   await seedUsers();
+  //   await seedCustomers();
+  //   await seedInvoices();
+  //   await seedRevenue();
+  //   await client.sql`COMMIT`;
+  //   return Response.json({ message: "Database seeded successfully" });
+  // } catch (error) {
+  //   await client.sql`ROLLBACK`;
+  //   return Response.json({ error }, { status: 500 });
+  // }
 }
