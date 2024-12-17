99ed4fd3085e0d85fc0181ef444abb9db4a995b4
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2024-12-17 14:02:37 +0900

    6章まで

--- app/page.tsx
+++ app/page.tsx
@@ -39,11 +39,11 @@ export default function Page() {
             alt='Screenshots of the dashboard project showing desktop version'
           />
           <Image
-            src='/hero-moble.png'
+            src='/hero-mobile.png'
             width={560}
             height={620}
             className='block md:hidden'
-            alt='Screenshots of the dashboard project showing mobile version'
+            alt='Screenshot of the dashboard project showing mobile version'
           />
         </div>
       </div>
